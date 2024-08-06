3be6cee1520b7d81f0d85372507a3d937d14ce92
kiaramaier <kiaramaier04@gmail.com>
2024-08-06 19:00:00 +1000

    added theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, createTheme } from '@mantine/core';
 import { HeaderSimple } from './components/Header/HeaderSimple';
 import { NewNotePage } from './features/NotePage/components/NewNotePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -9,9 +9,14 @@ import { Profile } from './features/Profile/Profile';
 import { GeneratedNote } from './features/NoteGeneration/GeneratedNote';
 import { ActivitiesBox } from './features/NotePage/components/ActivitiesBox';
 
+const theme = createTheme({
+  fontFamily: 'Open Sans, sans-serif',
+  primaryColor: 'violet',
+});
+
 function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Router>
         <HeaderSimple />
         <Routes>
